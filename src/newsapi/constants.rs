d9f095240c380546814f25442bb1c1010bac3cc8
biomunky <biomunky@gmail.com>
2019-03-10 00:12:52 +0000

    Allow dead code in constants

--- src/newsapi/constants.rs
+++ src/newsapi/constants.rs
@@ -1,8 +1,13 @@
 use std::collections::HashSet;
 use std::iter::FromIterator;
 
+#[allow(dead_code)]
 pub const TOP_HEADLINES_URL: &'static str = "https://newsapi.org/v2/top-headlines";
+
+#[allow(dead_code)]
 pub const EVERYTHING_URL: &'static str = "https://newsapi.org/v2/everything";
+
+#[allow(dead_code)]
 pub const SOURCES_URL: &'static str = "https://newsapi.org/v2/sources";
 
 lazy_static! {
