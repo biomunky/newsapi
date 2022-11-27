54871ef476fc6a9648c74581bf1420c53c183dea
Aleksandr-Gamble <aleksandr.gamble@gmail.com>
2022-11-27 09:33:48 -0500

    article.title Option<> for missing instances

--- src/payload/article.rs
+++ src/payload/article.rs
@@ -28,7 +28,7 @@ impl PartialEq for ArticleSource {
 pub struct Article {
     pub source: ArticleSource,
     pub author: Option<String>,
-    pub title: String,
+    pub title: Option<String>,      // This is missing on rare occasion
     pub description: Option<String>,
     pub url: String,
     #[serde(rename = "urlToImage")]
